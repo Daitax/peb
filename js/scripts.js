c2ee9490c7358298a37b291fa6358f80aed08644
Daitax <lars1515@yandex.ru>
2023-11-27 13:25:09 +0400

    Правка бага с зависимостью таймера от времени на устройстве

--- js/scripts.js
+++ js/scripts.js
@@ -216,36 +216,3 @@ if (finalTestingLink) {
         finalTestingLink.addEventListener('click', openPopup)
     }
 }
-
-let timer = document.querySelector('[test-elem="timer"]')
-
-if (timer) {
-    let timerId = null;
-    let endTimestamp = timer.getAttribute('end-time') * 1000
-    let endDate = new Date(endTimestamp)
-
-    let elemMinutes = timer.querySelector('[test-elem="minutes"]')
-    let elemSeconds = timer.querySelector('[test-elem="seconds"]')
-
-
-    function countdownTimer() {
-        let diff = endDate - new Date();
-
-        if (diff <= 0) {
-            clearInterval(timerId);
-        }
-
-        let minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
-        let seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
-
-        let formatMinutes = minutes < 10 ? '0' + minutes : minutes;
-        let formatSeconds = seconds < 10 ? '0' + seconds : seconds;
-
-        elemMinutes.innerHTML = formatMinutes
-        elemSeconds.innerHTML = formatSeconds
-    }
-
-    countdownTimer()
-
-    timerId = setInterval(countdownTimer, 1000);
-}
